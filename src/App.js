5545aef94d2e0fb3073b6760106d449c471483f8
BethRavelle <143979967+BethRavelle@users.noreply.github.com>
2023-11-29 14:12:14 +1000

    react set up and form shell

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Beth Ravelle</small>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
